1d86bbf6e9c9be3b4ed1b560f66b6a67d61fd7bb
ivan <398778994@qq.com>
2020-06-16 18:05:32 +0800

    demo

---
+++ build/static/js/7.2544fe27.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpc-react-antd"]=this["webpackJsonpc-react-antd"]||[]).push([[7],{97:function(t,n,a){"use strict";a.r(n);var c=a(0),e=a.n(c),r=a(71);n.default=function(t){var n=arguments.length>1&&void 0!==arguments[1]?arguments[1]:"views";return Object(r.a)((function(){return a(92)("./".concat(n,"/").concat(t))}),{fallback:e.a.createElement("div",null,"Loading...")})}}}]);
+//# sourceMappingURL=7.2544fe27.chunk.js.map
